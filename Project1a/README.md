1120810e4fe834f9eaf8b13339a8837eb79675f5
Karan Talreja <karantalreja91@gmail.com>
2016-09-08 16:55:38 -0500

    Added Sorting and Disk write
    Added benchmark refs

--- Project1a/README.md
+++ Project1a/README.md
@@ -1,7 +1,29 @@
 Benchmarks
-Run command: Command: ./bin/varsort -i gen_case/out/outfile.100.32 -o out 
-Read Only file: 174,088
+Run command: Command: ./bin/varsort -i gen_case/out/outfile.100.32 -o out
 
-Read and Create OM : 239,752
+Base Case: "return 0" only in main : 99,488
 
-Changed OM from list to Array : 197,545
+Read Only file: 174,088 (+74600 +74.98%)
+
+Read and Create OM : 239,752 (Upgraded)
+
+Changed OM from list to Array : 197,545 (Upgraded)
+
+Changed OM for Array of objects to Array of pointers : 217,745 (+43675 +25.08%)
+Cycles increased due to increased malloc calls.
+
+Sorting Added (qsort) : 249,745 (+32000 +14.69%)
+
+Added Write to disk : 293,772 (+44027 17.62%)
+
+So seeing the jumps, the pipeline comes out to be
+
+8th Sept 2016:
+
+BASE_CASE : 99488 (Can't do anything)
+READ_FILE : 74600
+CREATE_OM : 43675
+SORTING   : 32000
+WRITE     : 44027
+--------------------
+Total     : 293790
--- Project1a/src/varsort.c
+++ Project1a/src/varsort.c
@@ -7,7 +7,8 @@
 #include <string.h>
 #include "sort.h"
 
-int decompileArray (struct __rec_dataptr_t* inArray, unsigned int size);
+int decompileArray (struct __rec_dataptr_t** inArray, unsigned int size);
+int compareRecDataPtr (__const void* first, __const void* second);
 
 void usage(char *prog)
 {
@@ -46,7 +47,7 @@ int main(int argc, char *argv[])
   }
 
   // open output file
-  int fdOut = open(outFile, O_RDONLY);
+  int fdOut = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU);
   if (0 > fdOut)
   {
     perror("open");
@@ -66,7 +67,7 @@ int main(int argc, char *argv[])
   printf("Number of records: %d\n", recordsLeft);
   rec_nodata_t r;
   unsigned int count = 0;
-  struct __rec_dataptr_t* unsortedArray = (struct __rec_dataptr_t*)malloc(recordsLeft * sizeof(struct __rec_dataptr_t));
+  struct __rec_dataptr_t** unsortedArray = (struct __rec_dataptr_t**)malloc(recordsLeft * sizeof(struct __rec_dataptr_t*));
   while (recordsLeft)
   {
     // Read the fixed-sized portion of record: key and size of data
@@ -78,12 +79,13 @@ int main(int argc, char *argv[])
     }
     assert(r.data_ints <= MAX_DATA_INTS);
 
-    unsortedArray[count].key = r.key;
-    unsortedArray[count].data_ints = r.data_ints;
-    unsortedArray[count].data_ptr = (unsigned int*)malloc(r.data_ints*sizeof(unsigned int));
+    unsortedArray[count] = (struct __rec_dataptr_t*)malloc(sizeof(struct __rec_dataptr_t));
+    unsortedArray[count]->key = r.key;
+    unsortedArray[count]->data_ints = r.data_ints;
+    unsortedArray[count]->data_ptr = (unsigned int*)malloc(r.data_ints*sizeof(unsigned int));
 
     // Read the variable portion of the record
-    rc = read(fd, unsortedArray[count].data_ptr, r.data_ints * sizeof(unsigned int));
+    rc = read(fd, unsortedArray[count]->data_ptr, r.data_ints * sizeof(unsigned int));
     if (rc !=  r.data_ints * sizeof(unsigned int))
     {
       perror("read");
@@ -96,23 +98,64 @@ int main(int argc, char *argv[])
 
 //  decompileArray(unsortedArray, count);
 
+  qsort(&unsortedArray[0], count, sizeof(struct __rec_dataptr_t*), compareRecDataPtr);
+
+//  decompileArray(unsortedArray, count);
+  // output the number of keys as a header for this file
+  rc = write(fdOut, &count, sizeof(count));
+  if (rc != sizeof(count))
+  {
+    perror("write");
+    exit(1);
+  }
+  unsigned int i = 0;
+  struct __rec_data_t dumpRec;
+  while (i < count)
+  {
+	  dumpRec.key = unsortedArray[i]->key;
+	  dumpRec.data_ints = unsortedArray[i]->data_ints;
+	  unsigned int j;
+	  for (j = 0; j < unsortedArray[i]->data_ints; j++)
+		  dumpRec.data[j] = *(unsortedArray[i]->data_ptr + j);
+	  i++;
+	  int data_size = 2*sizeof(unsigned int) + dumpRec.data_ints*sizeof(unsigned int);
+
+	  rc = write(fdOut, &dumpRec, data_size);
+
+	  if (rc != data_size )
+	  {
+	    perror("write");
+	    exit(1);
+	  }
+  }
+
+
   (void)close(fd);
   (void)close(fdOut);
   return 0;
 }
 
-int decompileArray (struct __rec_dataptr_t* inArray, unsigned int size)
+int decompileArray (struct __rec_dataptr_t** inArray, unsigned int size)
 {
 	if (NULL == inArray) return 1;
 	unsigned int i = 0;
 	while (i < size)
 	{
-	    printf("key %d: %u data_ints: %u rec: ", i, inArray[i].key, inArray[i].data_ints);
+	    printf("key %d: %u data_ints: %u rec: ", i, inArray[i]->key, inArray[i]->data_ints);
 	    int j;
-	    for (j = 0; j < inArray[i].data_ints; j++)
-	      printf("%u ", *(inArray[i].data_ptr + j));
+	    for (j = 0; j < inArray[i]->data_ints; j++)
+	      printf("%u ", *(inArray[i]->data_ptr + j));
 	    printf("\n");
 	    i++;
 	}
+	return 0;
+}
+
+int compareRecDataPtr (__const void* first, __const void* seccond)
+{
+	struct __rec_dataptr_t* firstRecord = *(struct __rec_dataptr_t**)first;
+	struct __rec_dataptr_t* secondRecord = *(struct __rec_dataptr_t**)seccond;
+	if (firstRecord->key < secondRecord->key) return -1;
+	else if (firstRecord->key > secondRecord->key) return 1;
 	return 0;
 }
