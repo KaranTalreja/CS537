efe581180a0edb6127de393e9d868878dd85faad
Karan Talreja <karantalreja91@gmail.com>
2016-09-26 16:12:30 -0500

    Added support for exit builtin shell command

--- Project2a/mysh.c
+++ Project2a/mysh.c
@@ -16,6 +16,12 @@ typedef enum __mode {
   INTERACTIVE_MODE
 } mode_e;
 
+typedef enum __builtin {
+  EXIT_CMD,
+  J_CMD,
+  MYW_CMD
+} builtin_e;
+
 // @brief Function to print out usage of this program in case of invalid args
 void usage() {
   char* usageStr = "Usage: mysh [batchFile]\n";
@@ -27,6 +33,7 @@ int
 main(int argc, char* argv[]) {
   mode_e shellMode = INTERACTIVE_MODE;
   jobType_e jobType;
+  builtin_e builtinType;
   if (argc > 2) usage();
   FILE* input = (2 == argc) ? fopen(argv[1], "r") : stdin;
   if (NULL == input) {
@@ -53,28 +60,49 @@ main(int argc, char* argv[]) {
     if (0 == strcmp(argvs[i-1], "&")) {
       jobType = BACKGROUND_JOB;
       argvs[i-1] = NULL;
+      i--;
     } else {
       jobType = FOREGROUND_JOB;
       argvs[i] = NULL;
     }
-    int pid = fork();
-    if (0 == pid) {
-      if (BATCH_MODE == shellMode) {
-        int j = 0;
-        while (NULL != argvs[j]) {
-          write(STDOUT_FILENO, argvs[j], strlen(argvs[j]));
-          write(STDOUT_FILENO, " ", 1);
-          j++;
+    if (1 == i) {
+      if (0 == strcmp("exit", argvs[0])) {
+        jobType = BUILTIN_JOB;
+        builtinType = EXIT_CMD;
+      } else if (0 == strcmp("j", argvs[0])) {
+        jobType = BUILTIN_JOB;
+        builtinType = J_CMD;
+      } else if (0 == strcmp("myw", argvs[0])) {
+        jobType = BUILTIN_JOB;
+        builtinType = MYW_CMD;
+      }
+    }
+    if (BUILTIN_JOB != jobType) {
+      int pid = fork();
+      if (0 == pid) {
+        if (BATCH_MODE == shellMode) {
+          int j = 0;
+          while (NULL != argvs[j]) {
+            write(STDOUT_FILENO, argvs[j], strlen(argvs[j]));
+            write(STDOUT_FILENO, " ", 1);
+            j++;
+          }
+          write(STDOUT_FILENO, "\n", 1);
         }
+        execvp(argvs[0], argvs);
+        fprintf(stderr, "%s: Command not found\n", argvs[0]);
+        exit(1);
+      } else if (0 < pid) {
+        if (FOREGROUND_JOB == jobType) (void) wait(NULL);
+      } else {
+        write(STDERR_FILENO, "fork", 4);
+      }
+    } else {
+      if (BATCH_MODE == shellMode) {
+        write(STDOUT_FILENO, argvs[0], strlen(argvs[0]));
         write(STDOUT_FILENO, "\n", 1);
       }
-      execvp(argvs[0], argvs);
-      write(STDERR_FILENO, "execvp", 6);
-      exit(1);
-    } else if (0 < pid) {
-      if (FOREGROUND_JOB == jobType) (void) wait(NULL);
-    } else {
-      write(STDERR_FILENO, "fork", 4);
+      if (EXIT_CMD == builtinType) break;
     }
     if (INTERACTIVE_MODE == shellMode) write(STDOUT_FILENO, "mysh> ", 6);
   }
