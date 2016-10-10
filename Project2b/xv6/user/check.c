2fbec56f4ee0cedc99a0ed1252fa1684de785d1f
Karan Talreja <karan@cs.wisc.edu>
2016-10-10 12:26:02 -0500

    Project 2b: 10/10

--- Project2b/xv6/user/check.c
+++ Project2b/xv6/user/check.c
@@ -3,15 +3,77 @@
 #include "user.h"
 #include "pstat.h"
 
+#define check(exp, msg) if(exp) {} else {\
+  printf(1, "%s:%d check (" #exp ") failed: %s\n", __FILE__, __LINE__, msg);\
+  exit();}
+
+//#define DDEBUG 1
+
+#ifdef DDEBUG
+# define DEBUG_PRINT(x) printf x
+#else
+# define DEBUG_PRINT(x) do {} while (0)
+#endif
+
+int time_slices[] = {5, 5, 10, 20};
+
+int workload(int n) {
+  int i, j = 0;
+  for (i = 0; i < n; i++) {
+    j += i * j + 1;
+  }
+  return j;
+}
+
 int
 main(int argc, char *argv[])
 {
-  if(argc != 1){
-    printf(2, "Usage: check\n");
-    exit();
+  struct pstat st;
+
+  check(getpinfo(&st) == 0, "getpinfo");
+
+  // Push this thread to the bottom
+  workload(80000000);
+
+  int i, count = 0;
+
+  for (i = 0; i < 7; i++) {
+    int c_pid = fork();
+    // Child
+    if (c_pid == 0) {
+      workload(80000000);
+      sleep(100);
+      exit();
+    }   
   }
-  struct pstat processState;
-  getpinfo(&processState);
-  printf (1, "%d\n", processState.inuse[0]);
+
+  sleep(300);
+  check(getpinfo(&st) == 0, "getpinfo");
+
+  int j, k;
+  for (j = 0; j < NPROC; j++) {
+    if (st.inuse[j]) {
+      if (st.ticks[j][2] > time_slices[2] && st.ticks[j][2] % time_slices[2] == 0) {
+        count++;
+      }
+    
+      DEBUG_PRINT((1, "pid: %d\n", st.pid[j]));
+      for (k = 0; k < 4; k++) {
+        DEBUG_PRINT((1, "\t level %d ticks used %d\n", k, st.ticks[j][k]));
+      }
+    }
+  }
+
+  // Wait for child processes to finish..
+  for (j = 0; j < 6; j++) {
+    wait();
+  }
+
+  if (count) {
+    printf(1, "TEST PASSED");
+  } else {
+    printf(1, "TEST FAILED");
+  }
+
   exit();
 }
